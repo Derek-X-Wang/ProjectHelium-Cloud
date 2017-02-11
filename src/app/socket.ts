7a79fa6b0c7e1f954ce0cc9011095ccc1dd16fd9
Derek Wang <derekxinzhewang@gmail.com>
2017-02-11 05:58:42 -0800

    Heroku -> add Profile and change url

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -2,7 +2,7 @@ import * as actions from './actions';
 import * as io from 'socket.io-client';
 
 export default function (store:any) {
-  const socket = io.connect('http://localhost:3000');
+  const socket = io.connect('https://projecthelium-cloud.herokuapp.com:3000');
   socket.on('action', (message:string) => {
     store.dispatch(actions.addTodo(message));
   });
