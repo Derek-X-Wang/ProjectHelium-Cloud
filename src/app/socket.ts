c25a730b056ca3d8e3da50294104f10e7c164406
Derek Wang <derekxinzhewang@gmail.com>
2017-02-11 06:21:25 -0800

    Heroku -> update socket port

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -2,7 +2,7 @@ import * as actions from './actions';
 import * as io from 'socket.io-client';
 
 export default function (store:any) {
-  const socket = io.connect('https://projecthelium-cloud.herokuapp.com:3000');
+  const socket = io.connect('https://projecthelium-cloud.herokuapp.com');
   socket.on('action', (message:string) => {
     store.dispatch(actions.addTodo(message));
   });
